88b91c667a7325c1b95f2315ddbbb1acd1e772d9
Smeet Nagda <smeetnagda@Smeets-Air.lan>
2024-09-05 02:11:16 -0400

    add comments for UCS

--- ucs.py
+++ ucs.py
@@ -1,19 +1,23 @@
 import heapq
 
 def uniform_cost_search(road_network, start, goal):
+    # maintain queue to keep track of the cities and g(n) values
     priorityQ = []
     heapq.heappush(priorityQ, (0, start))
     originating_city = {}
     g_n = {start: 0}
 
     while priorityQ:
+        # pop the g(n) and the city from queue
         curr_city_g_n, curr_city = heapq.heappop(priorityQ)
-
+        # check if city is the goal
         if curr_city == goal:
             return reconstruct_path(originating_city, curr_city)
-
+        # go through neighboring cities
         for neighbor, distance in road_network.get_neighbors(curr_city):
+            # calculate temp g(n)
             temp = g_n[curr_city] + distance
+            # updat the values
             if neighbor not in g_n or temp < g_n[neighbor]:
                 originating_city[neighbor] = curr_city
                 g_n[neighbor] = temp
@@ -21,6 +25,7 @@ def uniform_cost_search(road_network, start, goal):
 
     return None
 
+# print Final path
 def reconstruct_path(originating_city, curr_city):
     final_path = [curr_city]
     while curr_city in originating_city:
